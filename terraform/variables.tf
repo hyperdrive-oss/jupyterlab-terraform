5e423a6fdaec5037cdac1ac52be2e77685f3c43b
Stanley.Zheng <stanley.zheng@cloudreach.com>
2018-03-04 09:35:17 -0500

    adding best practices for variable tags

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,9 +2,21 @@ variable "region" {
   default = "us-east-1"
 }
 variable "ami" {
+  # https://cloud-images.ubuntu.com/locator/ec2/
   description = "aws AMI number (default: Ubuntu 16.04LTS)"
   default = "ami-927185ef"
 }
+
+variable "default_tags" {
+  type = "map"
+  default = {
+    Name = "Jupyter Notebook Meganode"
+    Purpose = ""
+    KillDate = ""
+    Owner = ""
+  }
+}
+
 variable "keypair_name" {
   description = "MyKeyPair"
   default = "MyKeyPair"
